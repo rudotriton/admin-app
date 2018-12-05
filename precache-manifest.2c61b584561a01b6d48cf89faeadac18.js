a59a5f02af88e2f05ed6b09182a753fe1f62577d
Raigo Jerva <raigo.jerva@gmail.com>
2018-12-05 20:00:23 +0000

    Updates

---
+++ precache-manifest.2c61b584561a01b6d48cf89faeadac18.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ae5bebf50af64b067b4",
+    "url": "/admin-app/static/js/main.9ae5bebf.chunk.js"
+  },
+  {
+    "revision": "f4cd69441dba8cd4d4f9",
+    "url": "/admin-app/static/js/1.f4cd6944.chunk.js"
+  },
+  {
+    "revision": "e067cd28333d3b18c89b",
+    "url": "/admin-app/static/js/runtime~main.e067cd28.js"
+  },
+  {
+    "revision": "5f194c0e66059d997cfd6151bb7306d0",
+    "url": "/admin-app/index.html"
+  }
+];
